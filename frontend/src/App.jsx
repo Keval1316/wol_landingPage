7bbb42ba42cec8a8b0a23221fadcedf3e524c400
Keval Chudasama <kevalc1316@gmail.com>
2025-09-25 12:27:28 +0530

    Project Initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      {/* <Navbar />
+      <Hero /> */}
+      app
+    </div>
+  )
+}
+
+export default App
